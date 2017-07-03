9871b3fea411ea08bdb64d7cb82974572ca51401
Nozomi Morimoto <nozomi@NozominoMacBook-Pro.local>
2017-07-03 21:52:13 +0900

    fix

--- src/open_weather_map.rb
+++ src/open_weather_map.rb
@@ -1,5 +1,4 @@
 # Constants for OpenWeatherMap
 module OpenWeatherMap
   KEY = '3e69a122994192dbd9ef5e624d19b162'.freeze
-  BASE_URI = 'http://api.openweathermap.org/data/2.5/forecast'.freeze
 end
--- src/weather_api.rb
+++ src/weather_api.rb
@@ -4,13 +4,15 @@ require_relative './open_weather_map'
 require_relative './weather'
 
 # Access for OpenWeatherMap API
+
 class WeatherAPI
   def initialize
     @key = OpenWeatherMap::KEY
+    @bash_uri = 'http://api.openweathermap.org/data/2.5/forecast'.freeze
   end
 
   def weather(place)
-    uri = URI.parse(OpenWeatherMap::BASE_URI + query(place))
+    uri = URI.parse(@bash_uri + query(place))
     json = Net::HTTP.get(uri)
     hash = JSON.parse(json, symbolize_names: true)
     now = hash[:list][0]
