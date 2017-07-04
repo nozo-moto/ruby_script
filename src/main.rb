a5725467a122114ee95f9f5d434affd8ece9143b
Nozomi Morimoto <nozomi@wlan0422.u-aizu.ac.jp>
2017-07-04 13:53:53 +0900

    Fix some input output

--- src/main.rb
+++ src/main.rb
@@ -5,7 +5,7 @@ require_relative './schedule'
 if __FILE__ == $0
   puts "---------------\t Hello CalenderOfRuby \t ----------"
   loop do
-    print "| commands => #{Command::COMMAND_LIST}\n| input:\t"
+    print "| commands => #{Command::COMMAND_LIST}\n| input:\n"
     command = Commands.new(Command::COMMAND_LIST)
     command_input = gets.chomp()
     case command_input
